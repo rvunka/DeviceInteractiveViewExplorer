83d6591adc10de3e8a953a2f16e6f0cb368837a7
rvunka <moneygmcom@gmail.com>
2026-07-21 22:51:42 +0300

    Expose FindInterfaceProvider via DIVEComponentResolve.
    Thin forward to SharedComponentResolve for parity with ACTS/GRIP.

--- Source/DIVERuntime/Public/Utils/DIVEComponentResolve.h
+++ Source/DIVERuntime/Public/Utils/DIVEComponentResolve.h
@@ -25,4 +25,13 @@ namespace DIVEComponentResolve
 	{
 		return SharedComponentResolve::FindComponentByNameOrClass<ComponentType>(Owner, ComponentName);
 	}
+
+	template<typename InterfaceType>
+	UActorComponent* FindInterfaceProvider(
+		AActor* Owner,
+		FName ProviderName,
+		const UActorComponent* ExcludeComponent = nullptr)
+	{
+		return SharedComponentResolve::FindInterfaceProvider<InterfaceType>(Owner, ProviderName, ExcludeComponent);
+	}
 }
